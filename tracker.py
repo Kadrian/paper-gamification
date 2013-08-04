7aa738ff45739ce75ba1847c3893f45ccd5ecfba
Kai Rollmann <kai.rollmann@googlemail.com>
2013-08-04 23:40:44 +0200

    Add support for academic word list coverage

--- tracker.py
+++ tracker.py
@@ -3,18 +3,23 @@ import sys
 import time
 import operator
 import logging
+import requests
+import json
 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from watchdog.events import FileModifiedEvent
 
+
 class GamificationHandler(FileSystemEventHandler):
 	
-	def __init__(self, filename, publish_path):
+	def __init__(self, paper_filename, publish_url, paper_id):
+
 		FileSystemEventHandler.__init__(self)
 
-		self.filename = filename
-		self.publish_path = publish_path 
+		self.paper_filename = paper_filename 
+		self.publish_url = publish_url 
+		self.paper_id = paper_id
 
 		self.stats = {}
 		# words = {"hello": 5, "no": 3}
@@ -22,12 +27,12 @@ class GamificationHandler(FileSystemEventHandler):
 
 	def on_modified(self, event):
 		if type(event) == FileModifiedEvent:
-			if os.path.abspath(self.filename) == event.src_path:
-				self.register_change()
+			if os.path.abspath(self.paper_filename) == event.src_path:
+				self.calculate_statistics()
 				self.publish()
 
-	def register_change(self):
-		f = open(self.filename)
+	def calculate_statistics(self):
+		f = open(self.paper_filename)
 
 		avg_len = 0
 		num_words = 0
@@ -57,6 +62,10 @@ class GamificationHandler(FileSystemEventHandler):
 		# Determine Fancy word coverage
 		fancy_coverage = self.get_coverage("./fancy.txt")
 
+		# Determine academic word list coverage 
+		awl_coverage = self.get_awl_coverage("./awl.txt")
+
+		# Build stats together
 		self.stats = {
 			"num_words" : num_words,
 			"different_words" : len(self.words),
@@ -68,6 +77,12 @@ class GamificationHandler(FileSystemEventHandler):
 			"fancy_coverage" : {
 				"total" : fancy_coverage["total"],
 				"num_hits": len(fancy_coverage["hits"])
+			},
+			"awl_coverage" : {
+				"words_total": awl_coverage["words_total"],
+				"words_hits": awl_coverage["words_hits"],
+				"category_total": awl_coverage["category_total"],
+				"category_hits": awl_coverage["category_hits"]
 			}
 		}
 
@@ -101,35 +116,67 @@ class GamificationHandler(FileSystemEventHandler):
 		return { "total": num_words, "hits": list(hits)}
 
 
-	def publish(self):
-		f = open("./stats.txt", "w")
-		f.write(str(self.stats));
-		f.close()
-		os.system("scp ./stats.txt " + self.publish_path)
+	def get_awl_coverage(self, filename):
+		words = {}
+		f = open(filename)
 
+		category = ""
+		for word in f.readlines():
+			if not word.startswith('\t'):
+				category = word.strip()
+			words[word.strip()] = category
+
+		category_hits = set()
+		hits = set(words.keys()).intersection(set(self.words.keys()))
+		for hit in hits:
+			category_hits.add(words[hit])
+
+		return { 
+			"words_total": len(words), 
+			"words_hits": len(list(hits)), 
+			"category_total": len(list(set(words.values()))), 
+			"category_hits": len(category_hits)
+		}
+
+
+
+	def publish(self):
+		pass
+		# payload = {"stats" : json.dumps(self.stats)}
+		# r = requests.put(self.publish_url + "/papers/" + self.paper_id + ".json", params=payload)
+
+
+def set_paper_alive(publish_url, paper_id, alive):
+	pass
+	# payload = {"alive" : str(alive).lower()}
+	# r = requests.put(publish_url + "/papers/" + paper_id + ".json", params=payload)
 
 
 if __name__ == "__main__":
-
-	history_file = "history"
 	logging.basicConfig(level=logging.INFO,
 						format='%(asctime)s - %(message)s',
 						datefmt='%Y-%m-%d %H:%M:%S')
-	if len(sys.argv) != 3:
-		print "Usage: python tracker.py <file-to-be-watched> <scp-path-to-publish-to>"
+	if len(sys.argv) != 4:
+		print "Usage: python tracker.py <paper-file> <publish-host> <paper-id>"
 		sys.exit()
 
+	# Parse command line params
 	filename = sys.argv[1]
+	publish_url = sys.argv[2]
+	paper_id = sys.argv[3]
+
 	path = os.path.dirname(os.path.abspath(filename))
 
 	# Observer setup + start
-	event_handler = GamificationHandler(filename, sys.argv[2])
+	event_handler = GamificationHandler(filename, publish_url, paper_id)
 	observer = Observer()
 	observer.schedule(event_handler, path=path, recursive=True)
+	set_paper_alive(publish_url, paper_id, True);
 	observer.start()
 	try:
 		while True:
 			time.sleep(1)
 	except KeyboardInterrupt:
+		set_paper_alive(publish_url, paper_id, False);
 		observer.stop()
 	observer.join()
