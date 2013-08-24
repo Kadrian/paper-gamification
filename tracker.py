5c35737f82d96055fcf1566dc979e330a117effa
Kai Rollmann <kai.rollmann@googlemail.com>
2013-08-24 22:41:25 +0200

    Add interesting words calculation
    Minor change: Add a bunch of logging

--- tracker.py
+++ tracker.py
@@ -14,14 +14,22 @@ from watchdog.events import FileModifiedEvent
 
 
 class GamificationHandler(FileSystemEventHandler):
-	
+
 	def __init__(self, paper_filename, publish_url, paper_id):
 
 		FileSystemEventHandler.__init__(self)
 
-		self.paper_filename = paper_filename 
-		self.publish_url = publish_url 
+		self.paper_filename = paper_filename
+		self.publish_url = publish_url
 		self.paper_id = paper_id
+
+		logging.info("Creating a GamificationHandler with paper: " +
+			paper_filename +
+			" publish_url: " +
+			publish_url +
+			" and paper id: " +
+			paper_id
+		)
 
 		self.stats = {}
 		self.words = {}
@@ -31,10 +39,19 @@ class GamificationHandler(FileSystemEventHandler):
 
 	def on_modified(self, event):
 		# MAIN CALLBACK - a file got modified
+		logging.info("Modify event occurred: " + event.src_path)
 		if type(event) == FileModifiedEvent:
-			if os.path.abspath(self.paper_filename) == event.src_path:
+			logging.info("A file was modified: " + event.src_path)
+
+			paper_path = os.path.abspath(self.paper_filename)
+			logging.info("Checking if it was the paper: " + paper_path)
+
+			if paper_path == event.src_path:
+				logging.info("Paper change detected, calculating statistics ...")
 				self.calculate_statistics()
+				logging.info("Publishing ...")
 				self.publish()
+				logging.info("Published!")
 
 
 	def parse_text_statistics(self, text):
@@ -49,7 +66,7 @@ class GamificationHandler(FileSystemEventHandler):
 			if word not in self.words:
 				self.words[word] = 0
 			self.words[word] += 1
-			
+
 			# Count all words
 			self.num_words += 1
 
@@ -58,7 +75,6 @@ class GamificationHandler(FileSystemEventHandler):
 		# Read file
 		document = docx.opendocx(self.paper_filename)
 		text = " ".join(docx.getdocumenttext(document))
-		print text
 		word_split = re.findall(r"[\w']+", text)
 
 		# Analyse
@@ -85,6 +101,7 @@ class GamificationHandler(FileSystemEventHandler):
 		self.total_word_len = 0
 
 		# Parse file 
+		logging.info("\tParsing the paper ...")
 		if self.paper_filename.endswith(".docx"):
 			self.parse_word_file()
 		else:
@@ -92,23 +109,33 @@ class GamificationHandler(FileSystemEventHandler):
 
 		# By now, text-statistics should be saved in instance variables
 
+		# Determine interesting words
+		logging.info("\tCalculating interesting words ...")
+		interesting_words = self.get_interesting_words(40)
+
 		# Determine average word length 
+		logging.info("\tCalculating average word length ...")
 		avg_len = float(self.total_word_len) / float(self.num_words)
 
 		# Determine Oxford coverage
+		logging.info("\tCalculating oxford coverage ...")
 		oxford_coverage = self.get_coverage("./oxford.txt")
 
 		# Determine Fancy word coverage
+		logging.info("\tCalculating fancy words coverage ...")
 		fancy_coverage = self.get_coverage("./fancy.txt")
 
 		# Determine academic word list coverage 
+		logging.info("\tCalculating academic word list coverage ...")
 		awl_coverage = self.get_awl_coverage("./awl.txt")
 
 		# Build stats together
+		logging.info("\tBuilding stats together ...")
 		self.stats = {
 			"num_words" : self.num_words,
 			"different_words" : len(self.words),
 			"avg_len" : avg_len,
+			"interesting_words": interesting_words,
 			"oxford_coverage" : {
 				"total" : oxford_coverage["total"],
 				"num_hits": len(oxford_coverage["hits"])
@@ -128,7 +155,41 @@ class GamificationHandler(FileSystemEventHandler):
 
 		# Sort
 		#stats = sorted(words.iteritems(), key=operator.itemgetter(1), reverse=True)		
-		logging.info("Stats: " + str(self.stats))
+		logging.info("\tStats: " + str(self.stats))
+
+
+	def get_interesting_words(self, num):
+		sorted_words = sorted(self.words.iteritems(), key=operator.itemgetter(1), reverse=True)
+		interesting_words = []
+
+		num = min(num, len(sorted_words))
+		min_len = 10
+
+		while len(interesting_words) != num: # As long as we don't have as many words as we want
+			for word in sorted_words:
+				if len(word[0]) >= min_len:
+					if word[1] == 1:
+						# Word only occurs once in the text
+						# -> since sorted_words is sorted by occurrence:
+						#    break and go down with min word length
+						break
+					if word not in interesting_words:
+						interesting_words.append(word)
+				if len(interesting_words) == num:
+					# Got enough words, break will break both loops
+					break
+			min_len -= 1
+			if min_len < 2: # Text contains really few words, we just have to add them, until we have enough
+				for word in sorted_words:
+					if word not in interesting_words:
+						interesting_words.append(word)
+						if len(interesting_words) == num:
+							# Got enough words, break will break both loops
+							break
+
+		# Sort result and return
+		interesting_words = sorted(interesting_words, key=operator.itemgetter(1), reverse=True)
+		return interesting_words
 
 
 	def get_coverage(self, filename):
@@ -141,7 +202,7 @@ class GamificationHandler(FileSystemEventHandler):
 			if word.strip() != "":
 				words.append(word.strip().lower())
 				num_words += 1
-			
+
 		hits = set(words).intersection(set(self.words.keys()))
 		f.close()
 		return { "total": num_words, "hits": list(hits)}
@@ -171,10 +232,10 @@ class GamificationHandler(FileSystemEventHandler):
 			if category_hits[key] > 0:
 				category_num_hits += 1
 
-		return { 
-			"words_total": len(words), 
-			"words_hits": len(list(hits)), 
-			"category_total": len(list(set(words.values()))), 
+		return {
+			"words_total": len(words),
+			"words_hits": len(list(hits)),
+			"category_total": len(list(set(words.values()))),
 			"category_num_hits": category_num_hits,
 			"category_hits": category_hits
 		}
@@ -205,16 +266,23 @@ if __name__ == "__main__":
 
 	path = os.path.dirname(os.path.abspath(filename))
 
-	# Observer setup + start
+	# Enable "Currently writing..."
+	set_paper_alive(publish_url, paper_id, True);
+
+	# Observer setup
 	event_handler = GamificationHandler(filename, publish_url, paper_id)
 	observer = Observer()
+	logging.info("Starting observer with watch path: " + path)
 	observer.schedule(event_handler, path=path, recursive=True)
-	set_paper_alive(publish_url, paper_id, True);
+	# Observer start 
 	observer.start()
+	logging.info("Observer started.")
+
 	try:
 		while True:
 			time.sleep(1)
 	except KeyboardInterrupt:
+		# Disable "Currently writing..."
 		set_paper_alive(publish_url, paper_id, False);
 		observer.stop()
 	observer.join()
